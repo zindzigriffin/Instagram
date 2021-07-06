b1b45f953b03e5f108dc0e7a824bfe48d20f9f4a
Zindzi Mikal Kai Griffin <zindzigriffin@fb.com>
2021-07-06 13:29:37 -0700

    Icon + Launch Screen and created a Splash Screen

---
+++ app/src/main/java/com/example/instagram/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
